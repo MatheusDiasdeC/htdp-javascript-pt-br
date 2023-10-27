424a60abd72db99730343084c67e4a4352850f10
Matheus <mat10carv@gmail.com>
2023-10-27 10:50:45 -0300

    interface personagens implementada, make 'things' é o quue está sendo realizado

--- src/meu trabalho/Jogo.ts
+++ src/meu trabalho/Jogo.ts
@@ -11,17 +11,14 @@ import imgCoracaoUrl from "./Coracao.png";
 // - Como fazer os commits -> Nessa barra ateral -> Ícone que parece um grafo -> FAÇA os COMMITS!
 
 
-
-
 //Criação da Tela
 const [LARGURA, ALTURA] = [900, 600]
-
 const TELA = cenaVazia(LARGURA, ALTURA)
 
 
 //-----------------------------------------------------------------------------------------------------------
 
-
+//-- Criar Imagens --
 //Criar Tartaruga
 const IMG_TARTARUGA_LESTE = carregarImagem(imgTartarugaUrl, 110, 100)
 const IMG_TARTARUGA_OESTE = espelhar(IMG_TARTARUGA_LESTE)
@@ -30,16 +27,18 @@ const IMG_TARTARUGA_OESTE = espelhar(IMG_TARTARUGA_LESTE)
 const IMG_CARANGUEJO = carregarImagem(imgCaranguejoUrl, 110, 100)
 
 //Criar Gaivota
-const IMG_GAIVOTA = carregarImagem(imgGaivotaUrl, 100, 100)
+const IMG_GAIVOTA_OESTE = carregarImagem(imgGaivotaUrl, 100, 100)
+const IMG_GAIVOTA_LESTE = espelhar(IMG_GAIVOTA_OESTE)
 
 //Criar Coração
 const  IMG_CORACAO = carregarImagem(imgCoracaoUrl, 100, 100)
 
+
 //-----------------------------------------------------------------------------------------------------------
 
-
+//--Constantes de Posição--
 //Ver Imagem na Tela (Teste)
-//IMG_CARANGUEJO.desenha()
+IMG_CARANGUEJO.desenha()
 
 
 //Altura Inicial de Tartaruga
@@ -49,6 +48,7 @@ const Y_INICIAL_TARTARUGA = ALTURA / 2
 const Y_INICIAL_CARANGUEJO = 4 * (ALTURA / 5)
 
 //Altura Inicial de Gaivota
+const Y_INICIAL_GAIVOTA = 4 * (ALTURA / 5)
 
 //Limites da Tartaruga
 const LIMITE_ESQUERDA_TARTARUGA = 0 + larguraImagem(IMG_TARTARUGA_LESTE) / 2
@@ -56,15 +56,30 @@ const LIMITE_DIREITA_TARTARUGA = LARGURA - larguraImagem(IMG_TARTARUGA_LESTE) / 
 const LIMITE_BAIXO_TARTARUGA = ALTURA - larguraImagem(IMG_TARTARUGA_LESTE) / 2
 const LIMITE_CIMA_TARTARUGA = 0 + larguraImagem(IMG_TARTARUGA_LESTE) / 2
 
+//Limites dos Caranguejos
+const LIMITE_ESQUERDA_CARANGUEJO = 0 + larguraImagem(IMG_CARANGUEJO) / 2
+const LIMITE_DIREITA_CARANGUEJO = LARGURA - larguraImagem(IMG_CARANGUEJO) / 2
+const LIMITE_BAIXO_CARANGUEJO = ALTURA - larguraImagem(IMG_CARANGUEJO) / 2
+const LIMITE_CIMA_CARANGUEJO = 0 + larguraImagem(IMG_CARANGUEJO) / 2
+
+//Limites das Gaivotas
+const LIMITE_ESQUERDA_GAIVOTA = 0 + larguraImagem(IMG_GAIVOTA_LESTE) / 2
+const LIMITE_DIREITA_GAIVOTA = LARGURA - larguraImagem(IMG_GAIVOTA_LESTE) / 2
+const LIMITE_BAIXO_GAIVOTA = ALTURA - larguraImagem(IMG_GAIVOTA_LESTE) / 2
+const LIMITE_CIMA_GAIVOTA = 0 + larguraImagem(IMG_GAIVOTA_LESTE) / 2
+
 //Velocidade Inicial
 const D_PADRAO = 3
 
 
+//-----------------------------------------------------------------------------------------------------------
+//-- Interfaces --
+
 //Definição de Dados
 interface Jogo {
-    tart: Tartaruga,
-    caras: Caranguejo[],
-    gaivas: Gaivota[],
+    tart: Personagem,
+    caras: Personagem[],
+    gaivas: Personagem[],
     itensVida: ItemVida[],
     blocos: Bloco[],
     vidas: number
@@ -100,16 +115,44 @@ const TART_INICIAL = {
 }
 //dx e dy mudam no trata tecla
 
+const CARA_INICIAL = {
+    x: 2 * (LIMITE_ESQUERDA_CARANGUEJO / 5),
+    y: 4 * (ALTURA / 5),
+    dx: 0,
+    dy: D_PADRAO,
+
+    limiteCima: LIMITE_CIMA_CARANGUEJO,
+    limiteBaixo: LIMITE_BAIXO_CARANGUEJO,
+    limiteEsquerdo: LIMITE_ESQUERDA_CARANGUEJO,
+    limiteDireito: LIMITE_DIREITA_CARANGUEJO
+
+    //Alterar para "Limites Caranjguejo"
+}
+
+const GAIVOTA_INICIAL = {
+    x: 4 * (LIMITE_ESQUERDA_GAIVOTA / 5),
+    y: 4 * (ALTURA / 5),
+    dx: D_PADRAO,
+    dy: D_PADRAO,
+
+    limiteCima: LIMITE_CIMA_GAIVOTA,
+    limiteBaixo: LIMITE_BAIXO_GAIVOTA,
+    limiteEsquerdo: LIMITE_ESQUERDA_GAIVOTA,
+    limiteDireito: LIMITE_DIREITA_GAIVOTA
+
+    //Alterar para "Limites Gaivota"
+}
+
 //EXEMPLOS
-const CARA_VERTICAL = {x: 2 * (LARGURA / 5), y: LIMITE_CIMA_TARTARUGA, dx: 0, dy: D_PADRAO}
-const CARA_HORIZONTAL = {X: 2 * (LARGURA / 5), y: ALTURA / 3, dx: D_PADRAO, dy: 0}
-
-
-
+////const CARA_VERTICAL = {x: 2 * (LARGURA / 5), y: LIMITE_CIMA_TARTARUGA, dx: 0, dy: D_PADRAO}
+////const CARA_HORIZONTAL = {X: 2 * (LARGURA / 5), y: ALTURA / 3, dx: D_PADRAO, dy: 0}
 
 interface Bloco{
     x: number,
-    y: number
+    y: number,
+
+    altura: number,
+    largura: number
 }
 
 interface ItemVida {
@@ -117,43 +160,84 @@ interface ItemVida {
     y: number
 }
 
+//-----------------------------------------------------------------------------------------------------------
 
-
-//Cria um "objeto" T (Tartaruga) e C (Caranguejo)
+//Cria um "objeto" T (Tartaruga), C (Caranguejo) e G (Gaivota)
 function makeTartaruga(x: number, y: number, dx: number, dy: number): Personagem {
-    return { x: x, y: y, dx: dx, dy: dy,
-             limiteCima:  LIMITE_CIMA_TARTARUGA, limiteBaixo: LIMITE_BAIXO_TARTARUGA, limiteEsquerdo: LIMITE_ESQUERDA_TARTARUGA, limiteDireito: LIMITE_DIREITA_TARTARUGA};
+    return { x: x,
+            y: y,
+            dx: dx,
+            dy: dy,
+
+            limiteCima: LIMITE_CIMA_TARTARUGA,
+            limiteBaixo: LIMITE_BAIXO_TARTARUGA,
+            limiteEsquerdo: LIMITE_ESQUERDA_TARTARUGA,
+            limiteDireito: LIMITE_DIREITA_TARTARUGA};
 }
 
 function makeCaranguejo(x: number, y: number, dx: number, dy: number): Personagem {
-    return { x: x, y: y, dx: dx, dy: dy,
-             limiteCima:  LIMITE_CIMA_TARTARUGA, limiteBaixo: LIMITE_BAIXO_TARTARUGA, limiteEsquerdo: LIMITE_ESQUERDA_TARTARUGA, limiteDireito: LIMITE_DIREITA_TARTARUGA};
-             //Criar limites para caranguejos e gaivotas
+    return { x: x,
+            y: y,
+            dx: dx,
+            dy: dy,
+
+            limiteCima: LIMITE_CIMA_CARANGUEJO,
+            limiteBaixo: LIMITE_BAIXO_CARANGUEJO,
+            limiteEsquerdo: LIMITE_ESQUERDA_CARANGUEJO,
+            limiteDireito: LIMITE_DIREITA_CARANGUEJO};
+}
+
+function makeGaivota(x: number, y: number, dx: number, dy: number): Personagem {
+    return { x: x,
+            y: y,
+            dx: dx,
+            dy: dy,
+            
+            limiteCima: LIMITE_CIMA_GAIVOTA,
+            limiteBaixo: LIMITE_BAIXO_GAIVOTA,
+            limiteEsquerdo: LIMITE_ESQUERDA_GAIVOTA,
+            limiteDireito: LIMITE_DIREITA_GAIVOTA};
 }
 
 //Posições Iniciais de T
-const TARTARUGA_INICIAL = makeT(LIMITE_ESQUERDA_TARTARUGA, Y_INICIAL_TARTARUGA, DX_PADRAO, 0)
-const TARTARUGA_INICIAL2 = makeT(LIMITE_ESQUERDA_TARTARUGA + DX_PADRAO, Y_INICIAL_TARTARUGA, DX_PADRAO, 0)
+const TARTARUGA_INICIAL = makeTartaruga(LIMITE_ESQUERDA_TARTARUGA, Y_INICIAL_TARTARUGA, D_PADRAO, 0)
+    //Após 1 Tic
+const TARTARUGA_INICIAL2 = makeTartaruga(LIMITE_ESQUERDA_TARTARUGA + D_PADRAO, Y_INICIAL_TARTARUGA, D_PADRAO, 0)
 
-const TARTARUGA0 = makeT(LIMITE_ESQUERDA_TARTARUGA, Y_INICIAL_TARTARUGA, 3, 4)
-const TARTARUGA1 = makeT(LIMITE_ESQUERDA_TARTARUGA + 3, Y_INICIAL_TARTARUGA + 4, 3, 4)
+const TARTARUGA0 = makeTartaruga(LIMITE_ESQUERDA_TARTARUGA, Y_INICIAL_TARTARUGA, 3, 4)
+const TARTARUGA1 = makeTartaruga(LIMITE_ESQUERDA_TARTARUGA + 3, Y_INICIAL_TARTARUGA + 4, 3, 4)
 
+//Posições Iniciais de C
+const CARANGUEJO_01_INICIAL = makeCaranguejo(2 * (LARGURA / 5), Y_INICIAL_CARANGUEJO, 0, D_PADRAO)
+const Caranguejo_01_POSTERIOR = makeCaranguejo(2 * (LARGURA / 5) + D_PADRAO, Y_INICIAL_CARANGUEJO, 0, D_PADRAO)
 
-//const CARANGUEJO_INICIAL = makeC()
+const CARANGUEJO_02_INICIAL = makeCaranguejo(3 * (LARGURA / 5), Y_INICIAL_CARANGUEJO, 0, D_PADRAO)
+const Caranguejo_02_POSTERIOR = makeCaranguejo(3 * (LARGURA / 5) + D_PADRAO, Y_INICIAL_CARANGUEJO, 0, D_PADRAO)
 
-//const   CARANGUEJO_INICIAL = makeC()     !!!
+const CARANGUEJO_03_INICIAL = makeCaranguejo(LARGURA / 5, Y_INICIAL_CARANGUEJO, D_PADRAO, 0)
+const Caranguejo_03_POSTERIOR = makeCaranguejo(LARGURA / 5 + D_PADRAO, Y_INICIAL_CARANGUEJO, D_PADRAO, 0)
 
-// const T_MEIO = (x: LARGURA/2, y: 0, dx: 3, dy:0}
+//Posições Iniciais de G
+const GAIVOTA_01_INICIAL = makeGaivota(4* (LARGURA / 5), Y_INICIAL_GAIVOTA, 0, D_PADRAO)
+const GAIVOTA_01_POSTERIOR = makeGaivota(4 * (LARGURA / 5) + D_PADRAO, Y_INICIAL_GAIVOTA, 0, D_PADRAO)
 
-const TARTARUGA_FIM = makeT(LIMITE_DIREITA_TARTARUGA + 1, LIMITE_BAIXO_TARTARUGA, 3, 0)
-const TARTARUGA_VIRANDO = makeT(LIMITE_DIREITA_TARTARUGA, LIMITE_BAIXO_TARTARUGA, -3, 0)
-const TARTARUGA_VOLTANDO = makeT(LARGURA / 2, LIMITE_BAIXO_TARTARUGA, -3, 0)
+//-----------------------------------------------------------------------------------------------------------
+//Estou trabalhando AQUI!
+
+//Movimento de T ao chegar no final
+const TARTARUGA_FIM = makeTartaruga(LIMITE_DIREITA_TARTARUGA + 1, LIMITE_BAIXO_TARTARUGA, 3, 0)
+const TARTARUGA_VIRANDO = makeTartaruga(LIMITE_DIREITA_TARTARUGA, LIMITE_BAIXO_TARTARUGA, -3, 0)
+const TARTARUGA_VOLTANDO = makeTartaruga(LARGURA / 2, LIMITE_BAIXO_TARTARUGA, -3, 0)
+
+//Movimento de C ao chegar no final
+////const CARANGUEJO_FIM = makeCaranguejo(LIMITE_DIREITA_TARTARUGA + 1, LIMITE_BAIXO_TARTARUGA, 3, 0)
+//o QUE É ISSO? ^
 
 //Mover T
 
 //T -> t (Corrigir) (OK?)
 //Colocar os Testes (OK?)
-function moveT(tuga: Tartaruga): Tartaruga {
+function moveT(tuga: Personagem): Personagem {
     if (tuga.x > tuga.limiteDireito) {
         return { ...tuga, x: LIMITE_DIREITA_TARTARUGA, dx: -tuga.dx }
     } //... -> copia o objeto que vc quer copiar, e tudo o que vem depois da vírgula é o que você quer modificar
@@ -178,21 +262,21 @@ testes(() => {
                 expect(moveT(TARTARUGA_FIM)).toStrictEqual(TARTARUGA_VIRANDO);
             });
             test('move vaca limite esquerdo', () => {
-                expect(moveT(makeT(LIMITE_ESQUERDA_TARTARUGA - 1, ALTURA / 2, -3, 0)))
-                    .toStrictEqual(makeT(LIMITE_ESQUERDA_TARTARUGA, ALTURA / 2, 3, 0));
+                expect(moveT(makeTartaruga(LIMITE_ESQUERDA_TARTARUGA - 1, ALTURA / 2, -3, 0)))
+                    .toStrictEqual(makeTartaruga(LIMITE_ESQUERDA_TARTARUGA, ALTURA / 2, 3, 0));
             });
             test('move vaca limite baixo', () => {
-                expect(moveT(makeT(LARGURA / 2, LIMITE_BAIXO_TARTARUGA+1, 0, 3)))
-                    .toStrictEqual(makeT(LARGURA/2, LIMITE_BAIXO_TARTARUGA, 0, -3));
+                expect(moveT(makeTartaruga(LARGURA / 2, LIMITE_BAIXO_TARTARUGA+1, 0, 3)))
+                    .toStrictEqual(makeTartaruga(LARGURA/2, LIMITE_BAIXO_TARTARUGA, 0, -3));
             });
             test('move vaca limite cima', () => {
-                expect(moveT(makeT(LARGURA/2, LIMITE_CIMA_TARTARUGA-1, 0, -3)))
-                    .toStrictEqual(makeT(LARGURA/2, LIMITE_CIMA_TARTARUGA, 0, 3));
+                expect(moveT(makeTartaruga(LARGURA/2, LIMITE_CIMA_TARTARUGA-1, 0, -3)))
+                    .toStrictEqual(makeTartaruga(LARGURA/2, LIMITE_CIMA_TARTARUGA, 0, 3));
             });
         });
 })
 
-function desenhaT(tuga: Tartaruga): Imagem {
+function desenhaT(tuga: Personagem): Imagem {
     return colocarImagem(tuga.dx < 0? IMG_TARTARUGA_OESTE: IMG_TARTARUGA_LESTE, tuga.x, tuga.y, TELA);
 }
 testes(() => {
@@ -205,24 +289,24 @@ testes(() => {
 })
 
 
-function trataTeclaTartaruga(tuga: Tartaruga, tecla: string): Tartaruga {
+function trataTeclaTartaruga(tuga: Personagem, tecla: string): Personagem {
     if (tecla == "ArrowRight") {
-        return {...tuga, dx: DX_PADRAO, dy: 0}
+        return {...tuga, dx: D_PADRAO, dy: 0}
     }
     if (tecla == "ArrowLeft") {
-        return {...tuga, dx: -DX_PADRAO, dy: 0}
+        return {...tuga, dx: -D_PADRAO, dy: 0}
     }
     if (tecla == "ArrowDown") {
-        return {...tuga, dx: 0, dy: DX_PADRAO}
+        return {...tuga, dx: 0, dy: D_PADRAO}
     }
     if (tecla == "ArrowUp") {
-        return {...tuga, dx: 0, dy: -DX_PADRAO}
+        return {...tuga, dx: 0, dy: -D_PADRAO}
     }
     return tuga;
 }
 
 
-
+//-----------------------------------------------------------------------------------------------------------
 
 
 function main() {
